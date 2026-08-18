75c2c24dc981ca1a723faef97361070e9a72571d
Eduardo <edunogueira06@hotmail.com>
2026-08-18 11:53:42 -0300

    ex001

--- LAB/CAP 4/Parte 3 e 4/ex001.py
+++ LAB/CAP 4/Parte 3 e 4/ex001.py
@@ -1,4 +1,15 @@
 import numpy as np
 
 dataset = np.loadtxt('LAB/CAP 4/DataSets/space.csv', delimiter = ';', dtype = 'str', encoding = 'utf-8')
-print(dataset)
+#print(dataset)
+
+# Extrai a coluna de "Status Mission"
+dataset = dataset[1:,7]
+print(dataset)      # mascara
+
+# Gera uma mascara com true e false
+mascara = dataset == 'Success'
+
+# soma os trues e divide pelo tamanho do dataset
+media = np.sum(mascara) / len(dataset) * 100
+print(media)
