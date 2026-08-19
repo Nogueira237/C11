43bcff797b9b7f052445cb3dc0208d7197e0ba5d
Eduardo <edunogueira06@hotmail.com>
2026-08-18 21:23:44 -0300

    ex005.py

---
+++ LAB/CAP 4/Parte 3 e 4/ex005.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+dataset = np.loadtxt('LAB/CAP 4/DataSets/space.csv', delimiter = ';', dtype = 'str', encoding = 'utf-8')
+
+# extrai o nome das empresas
+dataset = dataset[1:, 1]
+
+# conjunto que retorna o nome das empresas e a quantidade de vezes que aparecem
+empresas, quantidade = np.unique(dataset, return_counts = True)
+
+for empresa, qtd in zip(empresas, quantidade):
+    print(f'Empresa: {empresa} -> {qtd} missões')
