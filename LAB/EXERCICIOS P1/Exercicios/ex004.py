d281522d8f056a6686492f61d07897829f0aa8b3
Eduardo <edunogueira06@hotmail.com>
2026-08-24 18:02:25 -0300

    ex004

---
+++ LAB/EXERCICIOS P1/Exercicios/ex004.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+dataset = np.loadtxt('LAB/EXERCICIOS P1/DataSets/paises.csv', delimiter = ';', dtype = 'str', encoding = 'utf-8')
+
+# extrai a coluna de regioes
+dataset = dataset[1:, 1]
+
+# 
+regioes = np.char.find(dataset, 'NORTHERN AMERICA') != 1
+
+print(f'Total de países da America do Norte: {sum(regioes)}')
