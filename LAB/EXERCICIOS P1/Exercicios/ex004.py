240983262c203560c69152da74ee0de7f44832d0
Eduardo <edunogueira06@hotmail.com>
2026-08-24 18:08:44 -0300

    ex004

--- LAB/EXERCICIOS P1/Exercicios/ex004.py
+++ LAB/EXERCICIOS P1/Exercicios/ex004.py
@@ -6,6 +6,6 @@ dataset = np.loadtxt('LAB/EXERCICIOS P1/DataSets/paises.csv', delimiter = ';', d
 dataset = dataset[1:, 1]
 
 # 
-regioes = np.char.find(dataset, 'NORTHERN AMERICA') != 1
+regioes = np.char.find(dataset, 'NORTHERN AMERICA') != -1
 
 print(f'Total de países da America do Norte: {sum(regioes)}')
