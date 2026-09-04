bd2399f14fb13058c1f64badeae3ede681862d01
Eduardo <edunogueira06@hotmail.com>
2026-09-04 12:02:18 -0300

    ex001

---
+++ LAB/EXERCICIOS EXTRA P1/ex001.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+control = 'Y'
+musicas = []
+
+while control == 'Y':
+    nome = input('Nome da música: ')
+    ano = int(input('Ano da música: '))
+    song = {'nome:': nome, 'ano': ano}
+    musicas.append(song)
+    control = input('Deseja adicionar outra música? [Y/N]')
+
+# a. Quantas músicas foram cadastradas;
+print(f"Número de músicas cadastradas: {len(musicas)}")
+
+# b. As Informações da(s) música(s) do ano mais antigo; 
+antiga =  min(musicas, key=lambda musica: musica['ano'])
+print(f"A música mais antiga é: {antiga}")
