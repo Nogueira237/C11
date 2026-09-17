25f4e2e46f4c13382b3defe0d8eef170894b6970
Eduardo <edunogueira06@hotmail.com>
2026-09-17 18:48:30 -0300

    ex007

---
+++ LAB/CAP 5/Parte3e4/ex007.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+dfPaises = pd.read_csv('LAB/CAP 5/DataSets/paises.csv', delimiter=';')
+
+# Função que reduz a mortalidade infantil em 15%
+def reduzirMortalidade(valor):
+    return valor * 0.85
+
+# Aplica a função à coluna
+mortalidadeReduzida = dfPaises['Infant mortality (per 1000 births)'].apply(reduzirMortalidade)
+
+# Concatena a coluna original com a coluna resultante
+comparacao = pd.concat([
+    dfPaises['Infant mortality (per 1000 births)'],
+    mortalidadeReduzida
+], axis=1)
+
+print(comparacao)
