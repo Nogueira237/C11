e17605136391ea120d5517cdca32eba30be5f8ef
Eduardo <edunogueira06@hotmail.com>
2026-09-17 18:50:01 -0300

    ex008

--- LAB/CAP 5/Parte3e4/ex008.py
+++ LAB/CAP 5/Parte3e4/ex008.py
@@ -2,8 +2,8 @@ import pandas as pd  # importa o pandas
 
 dfPaises = pd.read_csv('LAB/CAP 5/DataSets/paises.csv', delimiter = ';')    # le o dataset 'paises.csv'
 
-dfPaises = dfPaises.drop('Coastline (coast/area ratio)', axis=1)
+dfPaises = dfPaises.drop('Coastline (coast/area ratio)', axis=1)    # remove a coluna coastline
 
-dfPaises.to_csv('LAB/CAP 5/DataSets/paises_sem_coastline.csv', index=False)
+dfPaises.to_csv('LAB/CAP 5/DataSets/paises_sem_coastline.csv', index=False) # cria o .csv
 
 print(dfPaises)
