3174a7bace0ac182156153bd0772895b0951e15b
Eduardo <edunogueira06@hotmail.com>
2026-09-17 18:21:08 -0300

    ex002

---
+++ LAB/CAP 5/Parte3e4/ex002.py
@@ -0,0 +1,14 @@
+import pandas as pd  # importa o pandas
+import numpy as np  # importa o numpy
+
+dfPaises = pd.read_csv('LAB/CAP 5/DataSets/paises.csv', delimiter = ';')    # le o dataset 'paises.csv'
+
+dfPaises = dfPaises.loc[1:, ['Country', 'Region', 'Population']]
+
+Population = dfPaises.loc[:, 'Population']
+
+maior = np.argmax(Population)       # acha o indice do maior valor de 'population'
+
+print(f"Nome: {dfPaises['Country'][maior]}")
+print(f"Região: {dfPaises['Region'][maior]}")
+print(f"População: {dfPaises['Population'][maior]}")
