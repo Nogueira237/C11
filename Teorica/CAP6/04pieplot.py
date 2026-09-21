41e651df9251e06dddbd9c7161b9605bbf5362b9
Eduardo <edunogueira06@hotmail.com>
2026-09-21 18:45:17 -0300

    update: muda nome dos arquivos

---
+++ Teorica/CAP6/04pieplot.py
@@ -0,0 +1,10 @@
+# FUNDAMENTOS DE MATPLOTLIB
+import numpy as np                  # importa numpy
+import pandas as pd                 # importa pandas
+import matplotlib.pyplot as plt     # importa módulo 'pyploy' do matplotlib
+# Plotando gráficos de linhas com matplotlib (plot)
+
+ds = pd.read_csv('Teorica/DataSets/paises.csv', delimiter = ';')    # le o dataset 'paises.csv'
+
+## PIE PLOT
+
