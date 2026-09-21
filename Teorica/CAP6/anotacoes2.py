e0db6a41ae6dcd5d4860cf9e2c8082e6c1a7f54a
Eduardo <edunogueira06@hotmail.com>
2026-09-21 18:34:12 -0300

    create: scatter plot

--- Teorica/CAP6/anotacoes2.py
+++ Teorica/CAP6/anotacoes2.py
@@ -4,6 +4,19 @@ import pandas as pd                 # importa pandas
 import matplotlib.pyplot as plt     # importa módulo 'pyploy' do matplotlib
 # Plotando gráficos de linhas com matplotlib (plot)
 
-dfPaises = pd.read_csv('Teorica/DataSets/paises.csv', delimiter = ';')    # le o dataset 'paises.csv'
+ds = pd.read_csv('Teorica/DataSets/paises.csv', delimiter = ';')    # le o dataset 'paises.csv'
 
-print(dfPaises)
+# print(ds.columns)
+
+# Quao dispersas estao as rendas per capita dos 6 maiores paises do mundo?
+
+# pegando apenas os 6 maiores paises do planeta
+maioresPaises = ds.nlargest(6, 'Area (sq. mi.)')
+#print(maioresPaises)
+
+
+# Traçando o grafico com as rendas per capita desses paises
+plt.scatter(maioresPaises['Country'], maioresPaises['GDP ($ per capita)'], s = maioresPaises['Area (sq. mi.)']/10000)  # plt.scatter([linha], [coluna], s)
+# o parametro s permite adicionar uma terceira dimensao neste grafico, neste caso dividiu o parametro area por 10000^^
+
+plt.show()
