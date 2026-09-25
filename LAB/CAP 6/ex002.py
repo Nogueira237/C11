37d4c5a3ce3023f41a6d9bdc0921a44c2c92a696
Eduardo <edunogueira06@hotmail.com>
2026-09-25 11:06:31 -0300

    ex002

--- LAB/CAP 6/ex002.py
+++ LAB/CAP 6/ex002.py
@@ -2,5 +2,14 @@ import numpy as np                  # importa numpy
 import pandas as pd                 # importa pandas
 import matplotlib.pyplot as plt     # importa módulo 'pyploy' do matplotlib
 
-ds = pd.read_csv('LAB/CAP 6/DataSets/paises.csv', delimiter = ';')    # le o dataset 'paises.csv'
+ds = pd.read_csv('LAB/CAP 6/DataSets/space.csv', delimiter = ';')    # le o dataset 'paises.csv'
+
+ds = ds.drop_duplicates(subset='Company Name')  # e tira os duplicados
+
+usa = ds['Location'].str.contains('USA', na=False).shape[0]     # pega as empresas do eua
+china = ds[ds['Location'].str.contains('China', na=False)].shape[0]  # pega as empresas da china
+
+plt.bar(['EUA', 'China'], [usa, china])
+plt.show()
+
 
