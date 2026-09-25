0469c9251822db7593264dd9ee0d97b02576efc5
Eduardo <edunogueira06@hotmail.com>
2026-09-25 11:12:17 -0300

    ex003

--- LAB/CAP 6/ex003.py
+++ LAB/CAP 6/ex003.py
@@ -2,5 +2,14 @@ import numpy as np                  # importa numpy
 import pandas as pd                 # importa pandas
 import matplotlib.pyplot as plt     # importa módulo 'pyploy' do matplotlib
 
-ds = pd.read_csv('LAB/CAP 6/DataSets/paises.csv', delimiter = ';')    # le o dataset 'paises.csv'
+ds = pd.read_csv('LAB/CAP 6/DataSets/space.csv', delimiter = ';')    # le o dataset 'paises.csv'
+
+roscosmos = ds[ds['Company Name'] == 'Roscosmos']
+print(roscosmos)
+
+success = sum(roscosmos['Status Mission'] == 'Success')
+failure = len(roscosmos) - success
+
+plt.pie(x = [success, failure], labels = ['% Sucesso', '% Falha'], autopct = '%1.1f%%')
+plt.show()
 
