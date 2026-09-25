00005605043c28b36b7a7e30d337c0410d0c1fa9
Eduardo <edunogueira06@hotmail.com>
2026-09-25 11:17:08 -0300

    ex004

--- LAB/CAP 6/ex004.py
+++ LAB/CAP 6/ex004.py
@@ -2,5 +2,15 @@ import numpy as np                  # importa numpy
 import pandas as pd                 # importa pandas
 import matplotlib.pyplot as plt     # importa módulo 'pyploy' do matplotlib
 
-ds = pd.read_csv('LAB/CAP 6/DataSets/paises.csv', delimiter = ';')    # le o dataset 'paises.csv'
+ds = pd.read_csv('LAB/CAP 6/DataSets/space.csv', delimiter = ';')    # le o dataset 'paises.csv'
 
+falhas = ds[ds['Status Mission'] == 'Failure']      # pega somente as missoes falhas
+
+cincoMaiores = falhas['Company Name'].value_counts().head(5)    # pega as 5 empresas com mais falhas
+
+plt.bar(cincoMaiores.index, cincoMaiores.values)    # cria o gráfico
+
+plt.xlabel('Empresa')
+plt.ylabel('Número de falhas')
+
+plt.show()
